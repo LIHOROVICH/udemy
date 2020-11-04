a78454a53a186b236eb71681e7b28c4e03cfd841
Oleh Lykhorovych <42262191+LIHOROVICH@users.noreply.github.com>
2020-11-04 22:59:46 +0200

    second commit

--- udemy/views.py
+++ udemy/views.py
@@ -1,3 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 def index(request):
-	return HttpResponse(request.META)
+	#text = request['usertext']
+	return render(request, 'example.html', {'name': 'Oleh'})
